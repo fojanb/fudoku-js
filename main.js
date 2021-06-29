a551fd84d7607aefccd6f2d681fb80f4f41bbd65
Fojan Babaali <fozhanmail@gmail.com>
2021-06-28 22:31:34 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 // 6 by 6 sudoku
-let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let number = 0;
 let ranNums = [];
-let k = nums.length;
 
 const board = document.querySelector(".board");
 const gameBoard = () => {
   for (let i = 0; i <= 8; i++) {
     const subBoard = document.createElement("div");
     subBoard.classList.add("subBoard");
+    let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let k = nums.length;
+    let z = 0;
+    ranNums.length = 0;  //ranNums = [];
     while (k > 0) {
-      number = Math.floor(Math.random() * k);
-      ranNums.push(nums[number]);
-      nums.splice(number, 1);
+      z = Math.floor(Math.random() * k);
+      ranNums.push(nums[z]);
+      nums.splice(z, 1);
       k--;
     }
+    console.log(ranNums);
     for (let j = 0; j <= 8; j++) {
-      console.log(ranNums);
       const square = document.createElement("div");
       square.classList.add("squareStyle");
       square.innerText = ranNums[j];

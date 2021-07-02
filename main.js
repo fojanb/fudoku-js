158cacb84cd1fc6d5e81464111cf9efd8aafe201
Fojan Babaali <fozhanmail@gmail.com>
2021-07-02 12:37:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const board = document.querySelector(".board");
 // -------------------------------
 let ranNums = [8, 3, 1, 5, 6, 2, 9, 7, 4];
 let index = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
 let k;
 let z = 0;
 let b = 0;
